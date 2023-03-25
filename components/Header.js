366329b1718dd0c12875e5f4ff774e761d627c69
TQ-Zhang <ayan825@163.com>
2023-03-25 18:36:08 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -67,28 +67,9 @@ const Header = ({ navBarTitle, fullWidth }) => {
         <div className="flex items-center">
           <Link href="/" aria-label={BLOG.title}>
             <div className="h-6">
-              <a>TQ
-              </a>
-                <rect
-                  width="24"
-                  height="24"
-                  className="fill-current text-black dark:text-white"
-                />
-                <rect width="24" height="24" fill="url(#paint0_radial)" />
-                <defs>
-                  <radialGradient
-                    id="paint0_radial"
-                    cx="0"
-                    cy="0"
-                    r="1"
-                    gradientUnits="userSpaceOnUse"
-                    gradientTransform="rotate(45) scale(39.598)"
-                  >
-                    <stop stopColor="#CFCFCF" stopOpacity="0.6" />
-                    <stop offset="1" stopColor="#E9E9E9" stopOpacity="0" />
-                  </radialGradient>
-                </defs>
-              </svg>
+              <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="50" cy="50" r="44" stroke="#222222" stroke-width="12"/>
+</svg>
             </div>
           </Link>
           {navBarTitle ? (
