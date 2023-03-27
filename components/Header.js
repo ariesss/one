e7048393fa1c583e1bcfd0cd291878627634acee
TQ-Zhang <ayan825@163.com>
2023-03-27 21:32:59 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -7,8 +7,8 @@ const NavBar = () => {
   const locale = useLocale()
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.SHARING, to: '/sharing', show: BLOG.showSharing },
-    { id: 2, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
+    { id: 1, name: locale.NAV.SHARING, to: '/sharing', show: BLOG.true },
+    { id: 2, name: locale.NAV.ABOUT, to: '/about', show: BLOG.true },
     { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
